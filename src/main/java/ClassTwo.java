003531bb55bc1765473ebd07170dc642490a803e
Galyna Chernenko <kirsikka.gala@gmail.com>
2023-06-20 00:16:16 +0300

    Files edited

--- src/main/java/ClassTwo.java
+++ src/main/java/ClassTwo.java
@@ -3,7 +3,7 @@ public class ClassTwo
     int intExample = 127;
 
     String returnInt() {
-        System.out.println("Print from Class  \"ClassTwo\": " + intExample);
+        System.out.println("Print from Class \"ClassTwo\": " + intExample);
         return "Class \"ClassTwo\" returns: " + intExample;
     }
 }
