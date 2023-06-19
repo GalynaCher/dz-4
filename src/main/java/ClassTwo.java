303db039aa2f5cdd72d48ca25094351142c64ade
Galyna Chernenko <kirsikka.gala@gmail.com>
2023-06-20 00:12:49 +0300

    first commit

---
+++ src/main/java/ClassTwo.java
@@ -0,0 +1,9 @@
+public class ClassTwo
+{
+    int intExample = 127;
+
+    String returnInt() {
+        System.out.println("Print from Class  \"ClassTwo\": " + intExample);
+        return "Class \"ClassTwo\" returns: " + intExample;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    static public void main (String[] argv)
+    {
+        ClassTwo two = new ClassTwo();
+        System.out.println(two.returnInt());
+        System.out.println("var intExample: " + two.intExample);
+    }
+}
